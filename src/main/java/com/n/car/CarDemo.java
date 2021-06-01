5e9e72b69874f93e2caa13e0a7b3ec1a37d55a31
nicole <ganin0302@gmail.com>
2021-06-01 21:57:17 +0800

    CarDemo but I still not understand

---
+++ src/main/java/com/n/car/CarDemo.java
@@ -0,0 +1,15 @@
+package com.n.car;
+
+import javax.swing.*;
+
+public class CarDemo {
+    private JPanel main;
+    private JButton enterButton;
+    private JTextField PLEASEENTERYOURCARTextField;
+    private JRadioButton NORadioButton;
+    private JRadioButton YESRadioButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
